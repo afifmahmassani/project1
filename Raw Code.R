7a0bd31d0a189c9f8b9cb2449cce0691610e7df9
TurnerLuo <yitaoluo0701@gwu.edu>
2022-08-01 16:04:19 -0400

    Create Raw Code.R

---
+++ Raw Code.R
@@ -0,0 +1,31 @@
+library(ezids)
+knitr::opts_chunk$set(warning = F, results = "hide", message = F)
+options(scientific=T, digits = 3) 
+cardio_vascular_disease <- read.csv2("cardio_train.csv", header = TRUE)
+str(cardio_vascular_disease)
+
+df <- cardio_vascular_disease
+df$gender <- factor(cardio_vascular_disease$gender)
+df$smoke <- factor(cardio_vascular_disease$smoke)
+df$alco <- factor(cardio_vascular_disease$alco)
+df$active <- factor(cardio_vascular_disease$active)
+df$cardio <- factor(cardio_vascular_disease$cardio)
+df$cholesterol <- factor(cardio_vascular_disease$cholesterol)
+df$weight <- as.integer(cardio_vascular_disease$weight)
+df <- na.omit(df)
+df$id <- NULL
+str(df)
+
+df_num <- df
+
+for(i in 2:13){
+  if (!(i %in% c(2, 4:7, 9))){
+    df_num[,i] = as.numeric(df_num[,i])
+  }
+}
+cor <- cor(subset(df_num, select = -c(id)), method = "spearman")
+loadPkg("corrplot")
+corrplot(cor, type="lower", addCoef.col="black", number.cex=0.5, tl.cex=0.7,title="Cardio Vascular Disease Correlation", mar=c(0,0,1,0))
+
+library(bestglm)
+bestglm <- bestglm(df, IC = "AIC")
