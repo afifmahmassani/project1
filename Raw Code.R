4a04144cfa4fc5f92ccea549f45ceddb3fbd777c
TurnerLuo <yitaoluo0701@gwu.edu>
2022-08-01 22:11:44 -0400

    Update Raw Code.R

--- Raw Code.R
+++ Raw Code.R
@@ -1,9 +1,10 @@
 library(ezids)
 knitr::opts_chunk$set(warning = F, results = "hide", message = F)
 options(scientific=T, digits = 3) 
+#load data
 cardio_vascular_disease <- read.csv2("cardio_train.csv", header = TRUE)
 str(cardio_vascular_disease)
-
+#change variable types
 df <- cardio_vascular_disease
 df$gender <- factor(cardio_vascular_disease$gender)
 df$smoke <- factor(cardio_vascular_disease$smoke)
@@ -15,9 +16,8 @@ df$weight <- as.integer(cardio_vascular_disease$weight)
 df <- na.omit(df)
 df$id <- NULL
 str(df)
-
+#correlation plot
 df_num <- df
-
 for(i in 2:13){
   if (!(i %in% c(2, 4:7, 9))){
     df_num[,i] = as.numeric(df_num[,i])
@@ -26,6 +26,6 @@ for(i in 2:13){
 cor <- cor(subset(df_num, select = -c(id)), method = "spearman")
 loadPkg("corrplot")
 corrplot(cor, type="lower", addCoef.col="black", number.cex=0.5, tl.cex=0.7,title="Cardio Vascular Disease Correlation", mar=c(0,0,1,0))
-
+#logistic model
 library(bestglm)
 bestglm <- bestglm(df, IC = "AIC")
